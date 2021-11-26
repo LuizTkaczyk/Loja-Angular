29ee27c2d463cdf7b2683e52b7ea9b8a203ea4a1
LuizTkaczyk <luiz8907@hotmail.com>
2021-11-25 21:37:41 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loja';
+  nome = 'Luma';
+
+  public preco: number = 0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import {  DEFAULT_CURRENCY_CODE, LOCALE_ID,NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//import de moeda
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
